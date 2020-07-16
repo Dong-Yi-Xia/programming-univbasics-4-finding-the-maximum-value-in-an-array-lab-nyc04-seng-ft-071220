1f7a68ef77371e4f6b1d073b03b89885e1855b8b
Dong Xia <dong7777xia@gmail.com>
2020-07-16 16:10:41 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,13 +2,15 @@ def find_max_value(array)
 
   #array.sort.last
 
-  max = 0
-  array.each do |x|
-    if x > max
-      max = x
-    end
-  end
-  max
+  array.max
+
+  # max = 0
+  # array.each do |x|
+  #   if x > max
+  #     max = x
+  #   end
+  # end
+  # max
 
 
 
